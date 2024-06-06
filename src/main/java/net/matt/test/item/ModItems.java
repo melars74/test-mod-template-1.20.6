4657b37ade658e97bfd0ac37eb87aa4d3dc8e752
matt <melars@utexas.edu>
2024-06-06 14:26:02 -0500

    First Item(Box) added(fully working)!

---
+++ src/main/java/net/matt/test/item/ModItems.java
@@ -0,0 +1,28 @@
+package net.matt.test.item;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.matt.test.TestMod;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+
+    public static final Item BOX = registerItem("box", new Item(new Item.Settings()));
+
+    private static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries) {
+        entries.add(BOX);
+    }
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(TestMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        TestMod.LOGGER.info("Registering Mod Items for " + TestMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::addItemsToIngredientItemGroup);
+    }
+}
